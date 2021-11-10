1bb949fdba43bbcfafccb05694b81cdf21be1790
tuananh01011994 <43560681+tuananh01011994@users.noreply.github.com>
2021-11-10 08:44:32 +0700

    update run app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:crypto_mobile_application/src/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  print('Hello word');
+  runApp(MyApp());
 }
-
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,9 @@
+import 'package:crypto_mobile_application/src/screens/home_screen/home_screen.dart';
 import 'package:flutter/material.dart';
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }

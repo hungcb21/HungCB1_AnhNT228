053e7aba3d4995e13b4f801e59d10d12fa2101ee
tuananh01011994 <43560681+tuananh01011994@users.noreply.github.com>
2021-11-15 13:49:58 +0700

    fix url

--- test/widgets/coin_card_test.dart
+++ test/widgets/coin_card_test.dart
@@ -1,8 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:crypto_mobile_application/main.dart';
 
-void main() {
-}
+void main() {}

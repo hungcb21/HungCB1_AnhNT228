dbd61a7da83bc6612baa607207e5391350e7443a
tuananh01011994 <43560681+tuananh01011994@users.noreply.github.com>
2021-11-24 16:12:05 +0700

    fix name of variable

--- lib/src/screens/detail_screen/detail_screen.dart
+++ lib/src/screens/detail_screen/detail_screen.dart
@@ -116,7 +116,8 @@ class _DetailScreenState extends State<DetailScreen> {
                             Row(
                               mainAxisAlignment: MainAxisAlignment.start,
                               children: [
-                                Text(StringData.highIn24h, style: _coinInfo),
+                                Text(StringData.highPriceIn24h,
+                                    style: _coinInfo),
                                 Text(
                                     '${double.parse(_coins.high_24h.toString()).toStringAsFixed(2)}',
                                     style: _coinData),
@@ -125,7 +126,8 @@ class _DetailScreenState extends State<DetailScreen> {
                             Row(
                               mainAxisAlignment: MainAxisAlignment.start,
                               children: [
-                                Text(StringData.lowIn24h, style: _coinInfo),
+                                Text(StringData.lowPriceIn24h,
+                                    style: _coinInfo),
                                 Text(
                                     '${double.parse(_coins.low_24h.toString()).toStringAsFixed(2)}',
                                     style: _coinData),
